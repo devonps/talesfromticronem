b21d1ebc75f12df3c20af44dbf2eb76e469c90c1
Steven Devonport <steven.devonport@live.co.uk>
2019-01-02 18:03:59 +0000

    spells and condi components added

--- engine.py
+++ engine.py
@@ -1,23 +1,22 @@
-from newGame.initialiseNewGame import get_constants
+
 from utilities.jsonUtilities import read_json_file
 from loguru import logger
+from newGame import constants
 
 
 def main():
 
-    constants = get_constants()
-
-    logger.add(constants['logfile'], format=constants['logformat'])
+    logger.add(constants.LOGFILE, format=constants.LOGFORMAT)
 
     logger.info('********************')
     logger.info('* New game started *')
     logger.info('********************')
 
-    weapons = read_json_file(constants['Json_file_path'] + 'weapons.json')
+    weapons = read_json_file(constants.JSONFILEPATH + 'weapons.json')
     for weapon in weapons['weapons']:
         print(weapon['wielded_both_hands'])
 
-    mobiles = read_json_file(constants['Json_file_path'] + 'mobiles.json')
+    mobiles = read_json_file(constants.JSONFILEPATH + 'mobiles.json')
 
     for mobile in mobiles['mobiles']:
         print('The ' + mobile['name'] + ' is a type of ' + mobile['type'], )
@@ -28,13 +27,14 @@ def main():
                 print('The ' + mobile['name'] + ' is now a ' + mobile['name'] + ' ' + k['suffix'] +
                       ' and is now equipped with a ' + k['equipment'])
 
-    conditions = read_json_file(constants['Json_file_path'] + 'conditions.json')
+    conditions = read_json_file(constants.JSONFILEPATH + 'conditions.json')
     for condi in conditions['conditions']:
         print(condi['name'])
 
-    boons = read_json_file(constants['Json_file_path'] + 'boons.json')
+    boons = read_json_file(constants.JSONFILEPATH + 'boons.json')
     for boon in boons['boons']:
         print(boon['name'])
+
 
 if __name__ == '__main__':
     main()
