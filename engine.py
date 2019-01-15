b731189e6415b0dcb10a99fb62be0fc3cd7b6b36
Steven Devonport <steven.devonport@live.co.uk>
2019-01-15 15:56:24 +0000

    added create player character

--- engine.py
+++ engine.py
@@ -1,7 +1,8 @@
 
 from loguru import logger
-from newGame.initialiseNewGame import setup_game, generate_player_character
+from newGame.initialiseNewGame import setup_game, generate_player_character, load_weapon_with_spells
 from components import spells, weapons, mobiles
+from newGame.ClassWeapons import WeaponClass
 
 
 def main():
@@ -21,12 +22,33 @@ def main():
         spell_name_component = world.component_for_entity(slot.slot_five, spells.Name)
         print(name.label, spell_name_component.label)
 
+    # create the player
     player = generate_player_character(world)
     player_name = world.component_for_entity(player, mobiles.Name)
-    ai_level = world.component_for_entity(player, mobiles.AI)
+    ai_level_component = world.component_for_entity(player, mobiles.AI)
+    world.component_for_entity(player, mobiles.CharacterClass).label = 'necromancer'
+    class_component = world.component_for_entity(player, mobiles.CharacterClass)
+
+    # create a new weapon for the player
+    weapon = WeaponClass.create_weapon(world, 'sword')
+    weapon_type = world.component_for_entity(weapon, weapons.Name)
+    # parameters are: gameworld, weapon object, weapon type as a string, mobile class
+    load_weapon_with_spells(world, weapon, weapon_type.label, class_component.label)
+
+    # equip player with weapon
+    world.component_for_entity(player, mobiles.Equipped).both_hands = weapon
+
+    # confirm the weapon is equipped
+    weapon_equipped = world.component_for_entity(player, mobiles.Equipped).both_hands
+
+    wpn_name = world.component_for_entity(weapon_equipped, weapons.Name)
 
     print('Player name set as ' + player_name.first)
-    print('Player AI set to ' + str(ai_level.ailevel))
+    print('Player AI set to ' + str(ai_level_component.ailevel))
+    print('Current behaviour is ' + ai_level_component.behaviour)
+    print('Character class ' + class_component.label)
+    print('hands ' + str(wpn_name.label))
+
 
 if __name__ == '__main__':
     main()
