47313c7216f8caea0a3ba4b1f4e8016dab7a82ee
Steven Devonport <steven.devonport@live.co.uk>
2019-02-04 14:57:03 +0000

    additional map architecture stuff

--- map_objects/gameMap.py
+++ map_objects/gameMap.py
@@ -1,6 +1,7 @@
 import tcod
 
 from map_objects.tile import Tile
+from map_objects.rectangle import Rect
 
 
 class GameMap:
@@ -10,16 +11,25 @@ class GameMap:
         self.tiles = self.initialize_tiles()
 
     def initialize_tiles(self):
-        tiles = [[Tile(False) for y in range(self.height)] for x in range(self.width)]
-
-        tiles[30][22].block_path = True
-        tiles[30][22].block_sight = True
-        tiles[31][22].block_path = True
-        tiles[31][22].block_sight = True
-        tiles[32][22].block_path = True
-        tiles[32][22].block_sight = True
+        tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
 
         return tiles
+
+    def create_room(self, room):
+        for x in range(room.x1 + 1, room.x2):
+            for y in range(room.y1 + 1, room.y2):
+                self.tiles[x][y].block_path = False
+                self.tiles[x][y].block_sight = False
+
+    def create_h_tunnel(self, x1, x2, y):
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
+
+    def create_v_tunnel(self, y1, y2, x):
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
 
     def is_blocked(self,x ,y):
         if self.tiles[x][y].block_path:
@@ -30,5 +40,10 @@ class GameMap:
         This function creates the entire dungeon floor, including the bits that cannot be seen by the player
         :return:
         """
-        rooms = []
-        pass
+        room1 = Rect(20,15,10,15)
+        room2 = Rect(35,15,10,15)
+
+        self.create_room(room1)
+        self.create_room(room2)
+
+        self.create_h_tunnel(25,40,23)
