b8c11c8cfd1b9633cfffcde683dbab31b86abb12
Steven Devonport <steven.devonport@live.co.uk>
2019-02-07 17:20:38 +0000

    mouse controls added

--- utilities/display.py
+++ utilities/display.py
@@ -1,10 +1,15 @@
 import tcod
 
 
-def menu(con, header, options, width, screen_width, screen_height, posx, posy, foreground):
+def menu(con, header, options, width, screen_width, screen_height, posx, posy, foreground, key, mouse):
+
+    if len(options) > 26:
+        raise ValueError('Cannot have a menu with more than 26 options.')
 
     # calculate the total height for the menu header with one line per option
     header_height = tcod.console_get_height_rect(con, 0, 0, width, screen_height, header)
+    if header == '':
+        header_height = 0
     height = len(options) + header_height
 
     # create an off-screen window - this is where the menu options will be displayed
@@ -25,3 +30,33 @@ def menu(con, header, options, width, screen_width, screen_height, posx, posy, f
 
     # blit the contents of 'window' to the root console
     tcod.console_blit(window, 0, 0, width, height, 0, posx, posy, 1.0, 0.7)
+
+    # compute x and y offsets
+    x_offset = posx
+    y_offset = posy + header_height
+
+    while True:
+        tcod.console_flush()
+        tcod.sys_check_for_event(tcod.EVENT_KEY_PRESS | tcod.EVENT_MOUSE, key, mouse)
+
+        if mouse.lbutton_pressed:
+            (menu_x, menu_y) = (mouse.cx - x_offset, mouse.cy - y_offset)
+            if (menu_x >=0 and menu_x < width) and (menu_y >=0 and menu_y < height - header_height):
+                print('left mouse button pressed ' + str(menu_y))
+                print('menu option ' + chr(97 + menu_y))
+                return chr(97 + menu_y)
+
+        if mouse.rbutton_pressed or key.vk == tcod.KEY_ESCAPE:
+            return None
+
+        # convert the ASCII code to a menu option
+        index = key.c - ord('a')
+        key_char = chr(key.c)
+
+        if index >= 0 and index <= len(options):
+            return key_char
+
+        if index >= 0 and index <= 26:
+            return None
+
+
