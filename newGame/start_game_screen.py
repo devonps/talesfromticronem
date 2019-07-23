453e0c9c7e9601d69294a542acc0bfa3c2cb0f98
Steven Devonport <steven.devonport@live.co.uk>
2019-07-23 21:54:22 +0100

    Start menu works like other menus
    Start game menu acts like other character creation menus

--- newGame/start_game_screen.py
+++ newGame/start_game_screen.py
@@ -3,7 +3,9 @@ import tcod.console
 
 from loguru import logger
 from utilities import configUtilities
-from utilities.display import draw_colourful_frame, better_menu
+from utilities.display import draw_colourful_frame, pointy_menu
+from utilities.input_handlers import handle_game_keys
+from newGame.CharacterCreation import CharacterCreation
 
 
 class StartGame:
@@ -38,11 +40,6 @@ class StartGame:
                              corner_decorator='', corner_studs='',
                              msg='')
 
-        # place game menu options
-        better_menu(console=start_game_console, header='',
-                    menu_options=['New Game', 'Continue', 'Replay', 'Options', 'Help', 'Quit'], menu_id_format=True,
-                    menu_start_x=menu_start_x, blank_line=True)
-
         # place game version message
         start_game_console.print(x=version_x, y=version_y, string=game_version, fg=tcod.light_grey, bg=tcod.black, bg_blend=tcod.BKGND_DEFAULT)
 
@@ -50,9 +47,45 @@ class StartGame:
         start_game_console.print(x=copyright_x, y=copyright_y, string=game_copyright, fg=tcod.white, bg=tcod.black, bg_blend=tcod.BKGND_DEFAULT, alignment=tcod.LEFT)
         start_game_console.print(x=copyright_x + len(game_copyright) + 1, y=copyright_y, string=game_author, fg=tcod.yellow, bg=tcod.black, bg_blend=tcod.BKGND_DEFAULT, alignment=tcod.LEFT)
 
-        # blit changes to root console
-        start_game_console.blit(dest=root_console, dest_x=5, dest_y=5)
-        tcod.console_flush()
+        show_game_start_screen = True
+        selected_menu_option = 0
 
+        while show_game_start_screen:
 
+            pointy_menu(console=start_game_console, header='',
+                        menu_options=['New Game', 'Continue', 'Replay', 'Options', 'Help', 'Quit'], menu_id_format=True, menu_start_x=menu_start_x,
+                        menu_start_y=0,  blank_line=True, selected_option=selected_menu_option)
+
+            # blit changes to root console
+            start_game_console.blit(dest=root_console, dest_x=5, dest_y=5)
+            tcod.console_flush()
+
+            event_to_be_processed, event_action = handle_game_keys()
+            if event_to_be_processed != '':
+                if event_to_be_processed == 'keypress':
+                    if event_action == 'quit':
+                        raise SystemExit()
+                    if event_action == 'up':
+                        selected_menu_option -= 1
+                        if selected_menu_option < 0:
+                            selected_menu_option = 5
+                    if event_action == 'down':
+                        selected_menu_option += 1
+                        if selected_menu_option > 5:
+                            selected_menu_option = 0
+                    if event_action == 'enter':
+                        if selected_menu_option == 0:
+                            CharacterCreation.display_character_creation_options(root_console=root_console, game_config=game_config)
+                            root_console.clear(ch=32, fg=(0, 0, 0), bg=(0, 0, 0))
+                            StartGame.start_game_screen(root_console, game_config)
+                        if selected_menu_option == 1:     # continue existing game
+                            pass
+                        if selected_menu_option == 2:     # Replay old game
+                            pass
+                        if selected_menu_option == 3:     # Game options
+                            pass
+                        if selected_menu_option == 4:     # Help menu
+                            pass
+                        if selected_menu_option == 5:
+                            raise SystemExit()
 
