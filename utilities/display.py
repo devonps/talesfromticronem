6bbdab09d6c7d1653f92694ec7eac4444abe1af2
Steven Devonport <steven.devonport@live.co.uk>
2019-10-24 20:11:59 +0100

    BLT code added to new game screen
    Added Bearlibterminal to New Game screen
    Updated input_handlers to work with BLT
    Updated display_frame & pointy_menu to work with BLT
    Modified config.ini for BLT
    Updated colorutilities to work with strings not RGB components
    Started to use unicode for characters

--- utilities/display.py
+++ utilities/display.py
@@ -4,17 +4,19 @@ import tcod.console, tcod.event
 from utilities import configUtilities, colourUtilities
 from loguru import logger
 
+from bearlibterminal import terminal
+
 
 # the selected option is the choice from list_options that will be highlighted
 # so if list_options were  [apple, orange, grape] and selected_option were 'grape' then grape would be highlighted
 def coloured_list(console, list_options, list_x, list_y, selected_option, blank_line, fg):
     lst = 0
     list_count = 0
-    bg_color = colourUtilities.BLACK
+    bg_color = colourUtilities.get('BLACK')
 
     for option in list_options:
         if selected_option.lower() == option.lower():
-            fg_color = colourUtilities.YELLOW1
+            fg_color = colourUtilities.get('YELLOW1')
         else:
             fg_color = fg
 
@@ -27,30 +29,29 @@ def coloured_list(console, list_options, list_x, list_y, selected_option, blank_
             list_count += 1
 
 
-def pointy_menu(console, header, menu_options, menu_id_format, menu_start_x, menu_start_y, blank_line, selected_option):
+def pointy_menu(header, menu_options, menu_id_format, menu_start_x, menu_start_y, blank_line, selected_option):
     if len(menu_options) > 26:
         raise ValueError('Cannot have a menu with more than 26 options.')
 
     # print the header, with auto-wrap
     if header != '':
-        console.print(x=menu_start_x, y=10, string=header, bg_blend=tcod.BKGND_NONE, alignment=tcod.LEFT)
+        terminal.print_(x=menu_start_x, y=10, s=header)
 
     # print the menu options
     menu_count = 0
     mnu = 0
-    bg_color = colourUtilities.BLACK
 
     letter_index = ord('a')
     for option_text in menu_options:
         if selected_option == mnu:
-            fg_color = colourUtilities.YELLOW1
+            fg_color = colourUtilities.get('YELLOW1')
             mnu_pointer = '>'
         else:
-            fg_color = colourUtilities.WHITE
+            fg_color = colourUtilities.get('WHITE')
             mnu_pointer = ' '
-        men_text = mnu_pointer + ' ' + option_text
-        console.print(x=menu_start_x, y=menu_start_y + menu_count, string=men_text, fg=fg_color, bg=bg_color, bg_blend=tcod.BKGND_NONE,
-                      alignment=tcod.LEFT)
+        men_text = '[color=' + fg_color + ']' + mnu_pointer + ' ' + option_text
+
+        terminal.print_(x=menu_start_x, y=menu_start_y + menu_count, s=men_text)
         menu_count += 1
         mnu += 1
         if blank_line:
@@ -67,7 +68,7 @@ def display_coloured_box(console, title, posx, posy, width, height, fg, bg):
                       width=width - 2, height=height - 2, ch=0, fg=fg, bg=bg)
 
 
-def draw_colourful_frame(console, game_config, startx, starty, width, height, title, title_decorator, title_loc, corner_decorator, corner_studs, msg):
+def draw_colourful_frame(game_config, startx, starty, width, height, title, title_decorator, title_loc, corner_decorator, corner_studs, msg):
     # get config items
     root_con_width = configUtilities.get_config_value_as_integer(game_config, 'tcod', 'SCREEN_WIDTH')
     root_con_height = configUtilities.get_config_value_as_integer(game_config, 'tcod', 'SCREEN_HEIGHT')
@@ -77,99 +78,106 @@ def draw_colourful_frame(console, game_config, startx, starty, width, height, ti
         return
 
     # load glyphs for frames
-    gui_frame = configUtilities.get_config_value_as_string(configfile=game_config, section='gui',
-                                                           parameter='frame_border_pipe_type')
-    across_pipe = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
-                                                              parameter='frame_' + gui_frame + '_across_pipe')
-    bottom_left = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
-                                                              parameter='frame_' + gui_frame + '_bottom_left')
-    bottom_right = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
-                                                               parameter='frame_' + gui_frame + '_bottom_right')
-    down_pipe = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
-                                                            parameter='frame_' + gui_frame + '_down_pipe')
-    top_left = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
-                                                           parameter='frame_' + gui_frame + '_top_left')
-    top_right = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui', parameter='frame_' + gui_frame + '_top_right')
     msg_x = configUtilities.get_config_value_as_integer(configfile=game_config, section='newgame', parameter='PRETTY_FRAME_MSG_X')
     msg_y = configUtilities.get_config_value_as_integer(configfile=game_config, section='newgame', parameter='PRETTY_FRAME_MSG_Y')
 
-    fg = colourUtilities.WHITE
-    bg = colourUtilities.BLACK
-    corner_decorator_fg = colourUtilities.YELLOW1
-    corner_decorator_bg = colourUtilities.BLACK
+    fg = colourUtilities.get('YELLOW2')
+    bg = colourUtilities.get('BLACK')
 
     # draw basic frame
     #  top left corner
-    console.print(x=startx, y=starty, string=chr(top_left), fg=fg, bg=bg)
+    terminal.color(fg)
+    terminal.bkcolor(bg)
+
+    # unicode frame tiles
+    top_left = u'\u250c'
+    top_right = u'\u2510'
+    bottom_left = u'\u2514'
+    bottom_right = u'\u2518'
+    across_pipe = u'\u2500'
+    down_pipe = u'\u2502'
+
+    uni = u'\u2554'
+
+    # top left
+    terminal.put(x=startx, y=starty, c=top_left)
     # top left --> top right
-    console.draw_rect(x=startx + 1, y=starty, width=width - 7, height=1, ch=across_pipe, fg=fg, bg=bg)
+    pipe_across = (startx + 1) + width
+    for posx in range(pipe_across):
+        terminal.put(x=(startx + 1) + posx, y=starty, c=across_pipe)
     # top right
-    console.print(x=width-1, y=starty, string=chr(top_right), fg=fg, bg=bg)
+    terminal.put(x=(startx + 1) + pipe_across, y=starty, c=top_right)
     # right side down
-    console.draw_rect(x=width - 1, y=starty + 1, width=1, height=height - 4, ch=down_pipe, fg=fg, bg=bg)
-    # # right corner
-    console.print(x=width - 1, y=height - 1, string=chr(bottom_right), fg=fg, bg=bg)
-    # # bottom left --> bottom right
-    console.draw_rect(x=startx + 1, y=height - 1, width=width - 7, height=1, ch=across_pipe, fg=fg, bg=bg)
-    # # bottom left
-    console.print(x=startx, y=height - 1, string=chr(bottom_left), fg=fg, bg=bg)
-    # # left side down
-    console.draw_rect(x=startx, y=starty + 1, width=1, height=height - 4, ch=down_pipe, fg=fg, bg=bg)
-
+    pipe_down = height - starty - 2
+    for posy in range(pipe_down):
+        terminal.put(x=(startx + 1) + pipe_across, y=(starty + 1) + posy, c=down_pipe)
+    # right corner
+    terminal.put(x=(startx + 1) + pipe_across, y=height - 1, c=bottom_right)
+    # bottom left --> bottom right
+    for posx in range(pipe_across):
+        terminal.put(x=(startx + 1) + posx, y=height - 1, c=across_pipe)
+    # bottom left
+    terminal.put(x=startx, y=height - 1, c=bottom_left)
+    # left side down
+    for posy in range(pipe_down):
+        terminal.put(x=startx, y=(starty + 1) + posy, c=down_pipe)
+    title_edging = ''
     # draw string title + decorator if needed
-
     if title:
         if title_decorator:
-            title_decorator = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
-                                                                      parameter='frame_title_decorator')
+
+            title_edging = u'\u2502'  # vertical pipe
         pwx = 0
-        titlelen = len(title)
+        titlestring = title_edging + title + title_edging
+        titlelen = len(titlestring)
 
         if title_loc == 'left':
             pwx = startx + 3
         if title_loc == 'centre':
             titleminuspanel = width - titlelen
             pwx = int(titleminuspanel / 2)
+
         if title_loc == 'right':
             pwx = (width - titlelen) - 4
 
-        titlestring = chr(title_decorator) + title + chr(title_decorator)
-
-        console.print_box(x=pwx, y=starty, width=len(titlestring), height=1, string=titlestring)
+        terminal.print_(x=pwx, y=starty, s=titlestring)
 
     if msg != '':
-        s = msg.split('/')
-        console.print(x=msg_x, y=msg_y, fg=tcod.yellow, string=s[0])
-        console.print(x=msg_x + len(s[0]), y=msg_y, fg=tcod.white, string=s[1])
+        ss = msg.split('/')
+        terminal.print_(x=msg_x, y=msg_y, s=ss[0])
+        terminal.print_(x=msg_x + len(ss[0]), y=msg_y, s=ss[1])
 
     # You can only draw corner decorators or studs
     if corner_decorator != '':
-        cd = configUtilities.get_config_value_as_list(configfile=game_config, section='gui',
-                                                      parameter='frame_corner_decorator_' + corner_decorator)
 
-        corner_decorator = list(map(int, cd))
+        # unicode frame tiles
+        arc_top_left = u'\u25DC'
+        arc_top_right = u'\u256E'
+        arc_bottom_left = u'\u2570'
+        arc_bottom_right = u'\u256F'
+
         # top left corner
-        console.print(x=startx, y=starty, string=chr(corner_decorator[0]), fg=fg, bg=bg)
-        # top right corner
-        console.print(x=width-1, y=starty, string=chr(corner_decorator[1]), fg=fg, bg=bg)
-        # bottom left corner
-        console.print(x=startx, y=height - 1, string=chr(corner_decorator[2]), fg=fg, bg=bg)
-        # right corner corner
-        console.print(x=width - 1, y=height - 1, string=chr(corner_decorator[3]), fg=fg, bg=bg)
+        terminal.put(x=startx, y=starty, c=arc_top_left)
+        # # top right corner
+        terminal.put(x=width-1, y=starty, c=arc_top_right)
+        # # bottom left corner
+        terminal.put(x=startx, y=height - 1, c=arc_bottom_left)
+        # # right corner corner
+        terminal.put(x=width - 1, y=height - 1, c=arc_bottom_right)
 
     elif corner_studs != '':
             corner_stud_decorator = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
                                                                                 parameter='frame_corner_studs_' + corner_studs)
-            # top left
-            console.print(x=startx + 1, y=starty + 1, string=chr(corner_stud_decorator), fg=corner_decorator_fg, bg=corner_decorator_bg)
-            # top right
-            console.print(x=width - 2, y=starty + 1, string=chr(corner_stud_decorator), fg=corner_decorator_fg, bg=corner_decorator_bg)
-            # bottom left
-            console.print(x=startx + 1, y=height - 2, string=chr(corner_stud_decorator), fg=corner_decorator_fg, bg=corner_decorator_bg)
-            # bottom right
-            console.print(x=width - 2, y=height - 2, string=chr(corner_stud_decorator), fg=corner_decorator_fg, bg=corner_decorator_bg)
+            # # top left
+            # console.print(x=startx + 1, y=starty + 1, string=chr(corner_stud_decorator), fg=corner_decorator_fg, bg=corner_decorator_bg)
+            # # top right
+            # console.print(x=width - 2, y=starty + 1, string=chr(corner_stud_decorator), fg=corner_decorator_fg, bg=corner_decorator_bg)
+            # # bottom left
+            # console.print(x=startx + 1, y=height - 2, string=chr(corner_stud_decorator), fg=corner_decorator_fg, bg=corner_decorator_bg)
+            # # bottom right
+            # console.print(x=width - 2, y=height - 2, string=chr(corner_stud_decorator), fg=corner_decorator_fg, bg=corner_decorator_bg)
 
 
 def draw_clear_text_box(console, posx, posy, width, height, text, fg, bg):
-    console.draw_rect(x=posx, y=posy, width=width, height=height, ch=32, fg=colourUtilities.BLACK)
+    console.draw_rect(x=posx, y=posy, width=width, height=height, ch=32, fg=colourUtilities.get('BLACK'))
     console.print_box(x=posx, y=posy, width=width, height=height, string=text, fg=fg, bg=bg)
--- utilities/input_handlers.py
+++ utilities/input_handlers.py
@@ -1,73 +1,32 @@
-import tcod
-import tcod.event
 
-from components import mobiles, userInput
-from loguru import logger
-
-
-def handle_menus(key, mouse, gameworld):
-    key_char = chr(key.c)
-    if key.vk == tcod.KEY_CHAR:
-        return key_char
-    player_input_entity = get_user_input_entity(gameworld)
-    mouse_component = gameworld.component_for_entity(player_input_entity, userInput.Mouse)
-    keyboard_component = gameworld.component_for_entity(player_input_entity, userInput.Keyboard)
-    if mouse:
-        if mouse_component.lbutton:
-            return keyboard_component.keypressed
-    return {}
-
-
-def handle_mouse_in_menus(mouse, width, height, header_height, x_offset, y_offset):
-    if mouse.lbutton_pressed:
-
-        (menu_x, menu_y) = (mouse.cx - x_offset, mouse.cy - y_offset)
-        logger.info('left mouse button pressed at {}/{}', menu_x, menu_y)
-        if (0 <= menu_x < width) and (0 <= menu_y < height - header_height):
-            return menu_y
-    return -1
-
-
-def get_user_input_entity(gameworld):
-    for ent, (k, m) in gameworld.get_components(userInput.Mouse, userInput.Keyboard):
-        return ent
+from bearlibterminal import terminal
 
 
 def handle_game_keys():
     action = ''
     myevent = ''
-    for event in tcod.event.wait():
-        if event.type == 'KEYDOWN':
-            if event.sym == tcod.event.K_ESCAPE:
-                myevent = 'keypress'
-                action = 'quit'
-            if event.sym == tcod.event.K_LEFT:
-                myevent = 'keypress'
-                action = 'left'
-            if event.sym == tcod.event.K_RIGHT:
-                myevent = 'keypress'
-                action = 'right'
-            if event.sym == tcod.event.K_UP:
-                myevent = 'keypress'
-                action = 'up'
-            if event.sym == tcod.event.K_DOWN:
-                myevent = 'keypress'
-                action = 'down'
-            if event.sym == tcod.event.K_RETURN:
-                myevent = 'keypress'
-                action = 'enter'
-            if event.sym == tcod.event.K_BACKSPACE:
-                myevent = 'keypress'
-                action = 'delete'
-        if event.type == "TEXTINPUT":
-            myevent = 'textinput'
-            action = event.text
-        elif event.type == "MOUSEBUTTONDOWN":
-            myevent = 'mousebutton'
-            if event.button == tcod.event.BUTTON_LEFT:
-                action = ('left', event.tile.x, event.tile.y)
-            else:
-                action = ('right', event.tile.x, event.tile.y)
-        elif event.type == "MOUSEMOTION":
-            pass
+
+    key = terminal.read()
+    if key == terminal.TK_ESCAPE:
+        myevent = 'keypress'
+        action = 'quit'
+    if key == terminal.TK_UP:
+        myevent = 'keypress'
+        action = 'up'
+    if key == terminal.TK_DOWN:
+        myevent = 'keypress'
+        action = 'down'
+    if key == terminal.TK_ENTER:
+        myevent = 'keypress'
+        action = 'enter'
+    if key == terminal.TK_MOUSE_MOVE:
+        myevent = 'mousemove'
+        action = (terminal.TK_MOUSE_X, terminal.TK_MOUSE_Y)
+    if key == terminal.TK_MOUSE_LEFT:
+        myevent = 'mouseleftbutton'
+        action = (terminal.TK_MOUSE_X, terminal.TK_MOUSE_Y)
+    if key == terminal.TK_MOUSE_RIGHT:
+        myevent = 'mouserightbutton'
+        action = (terminal.TK_MOUSE_X, terminal.TK_MOUSE_Y)
+
     return myevent, action
