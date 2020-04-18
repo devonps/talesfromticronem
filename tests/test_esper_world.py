578a598abcd6d2d74f409b3e1d4666cfbf726cea
Steven Devonport <steven.devonport@live.co.uk>
2020-04-18 17:18:05 +0100

    Minor updates
    Refactored unit tests
    removed properties file for sonarcloud

--- tests/test_esper_world.py
+++ tests/test_esper_world.py
@@ -7,27 +7,42 @@ def world():
     return esper.World()
 
 
+class Position:
+    def __init__(self):
+        self.posx = 1
+        self.posy = 1
+
+
+class Health:
+    def __init__(self):
+        self.current = 100
+
+
 def test_create_world(world):
     assert type(world) == esper.World
 
 
 def test_create_entity(world):
-    e1 = world.create_entity()
-    e2 = world.create_entity()
-    assert type(e1) == int
-    assert type(e2) == int
-    assert e1 < e2
+    player = world.create_entity()
+    assert type(player) == int
 
 
-def test_entity_has_one_component(world):
-    e1 = world.create_entity()
-    e2 = world.create_entity()
-    world.add_component(e1, ComponentA())
-    world.add_component(e2, ComponentB())
-    assert world.has_component(e1, ComponentA) is True
-    assert world.has_component(e1, ComponentB) is False
-    assert world.has_component(e2, ComponentA) is False
-    assert world.has_component(e2, ComponentB) is True
+def test_two_entities_cannot_share_the_same_id(world):
+    player = world.create_entity()
+    enemy = world.create_entity()
+    assert player < enemy
+
+
+def test_entity_can_be_assigned_a_component(world):
+    player = world.create_entity()
+    world.add_component(player, Position())
+    assert world.has_component(player, Position) is True
+
+
+def test_entity_does_not_have_the_position_component(world):
+    enemy = world.create_entity()
+    world.add_component(enemy, Health())
+    assert world.has_component(enemy, Position) is False
 
 
 def test_clear_database(world):
@@ -37,16 +52,3 @@ def test_clear_database(world):
     assert len(world._processors) == 0
     assert len(world._dead_entities) == 0
     assert world._next_entity_id == 0
-
-
-# helpers
-
-class ComponentA:
-    def __init__(self):
-        self.posx = 1
-        self.posy = 1
-
-
-class ComponentB:
-    def __init__(self):
-        self.health = 100
