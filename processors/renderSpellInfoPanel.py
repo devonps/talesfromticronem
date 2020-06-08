0e048050e704421419adf775f514b26b8d49e3a8
Steven Devonport <steven.devonport@live.co.uk>
2020-06-08 21:06:51 +0100

    spellinfo bar equipped weapons

--- processors/renderSpellInfoPanel.py
+++ processors/renderSpellInfoPanel.py
@@ -3,6 +3,7 @@ from bearlibterminal import terminal
 
 from utilities import configUtilities, formulas
 from utilities.common import CommonUtils
+from utilities.itemsHelp import ItemUtilities
 from utilities.mobileHelp import MobileUtilities
 from utilities.spellHelp import SpellUtilities
 
@@ -10,19 +11,17 @@ from utilities.spellHelp import SpellUtilities
 class RenderSpellInfoPanel(esper.Processor):
     def __init__(self, gameworld):
         self.gameworld = gameworld
+        self.game_config = configUtilities.load_config()
 
     def process(self, game_config):
-        self.render_spell_infobox()
-        # self.render_player_status_effects(game_config=game_config)
-        # self.render_spell_info_panel(game_config=game_config)
-        # self.render_player_vitals(game_config=game_config)
+        self.render_spell_info_outer_frame()
+        self.render_equipped_items()
+        self.render_energy_bars()
+        self.render_player_status_effects(game_config=game_config)
+        self.render_class_mechanics()
+        self.render_spell_bar()
 
     def render_player_status_effects(self, game_config):
-        image_y_scale = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
-                                                                    parameter='map_Yscale')
-
-        statusbox_height = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
-                                                                       parameter='STATUSBOX_HEIGHT')
 
         player_entity = MobileUtilities.get_player_entity(gameworld=self.gameworld, game_config=game_config)
         player_boons = MobileUtilities.get_current_boons_applied_to_mobile(gameworld=self.gameworld,
@@ -30,33 +29,32 @@ class RenderSpellInfoPanel(esper.Processor):
         player_conditions = MobileUtilities.get_current_condis_applied_to_mobile(gameworld=self.gameworld,
                                                                                  entity=player_entity)
 
-        self.render_boons(posx=1, posy=statusbox_height * image_y_scale, list_of_boons=player_boons)
-        self.render_conditions(posx=11, posy=statusbox_height * image_y_scale,
-                               list_of_conditions=player_conditions)
+        self.render_boons(list_of_boons=player_boons)
+        self.render_conditions(list_of_conditions=player_conditions)
+
+    def render_equipped_items(self):
+        self.render_equipped_weapons(gameworld=self.gameworld, game_config=self.game_config)
+        self.render_equipped_jewellery()
+        self.render_equipped_armour()
+
+    def render_energy_bars(self):
+        self.render_health()
+        self.render_mana()
 
     @staticmethod
-    def render_boons(posx, posy, list_of_boons):
-        image_count = 0
-        image_scale_factor = 2
+    def render_boons(list_of_boons):
 
         for boon in list_of_boons:
-            boon_image_id = int(boon['image'])
-            terminal.put(x=(posx + image_count) * image_scale_factor, y=posy, c=0xE600 + boon_image_id)
-            image_count += 1
+            pass
 
     @staticmethod
-    def render_conditions(posx, posy, list_of_conditions):
-
-        image_count = 0
-        image_scale_factor = 2
+    def render_conditions(list_of_conditions):
 
         for condition in list_of_conditions:
-            condition_image_id = int(condition['image'])
-            terminal.put(x=(posx + image_count) * image_scale_factor, y=posy, c=0xE630 + condition_image_id)
-            image_count += 1
+            pass
 
     @staticmethod
-    def render_spell_infobox():
+    def render_spell_info_outer_frame():
 
         unicode_string_to_print = '[font=dungeon][color=SPELLINFO_FRAME_COLOUR]['
 
@@ -74,143 +72,107 @@ class RenderSpellInfoPanel(esper.Processor):
                                                                           parameter='SI_WIDTH')
         spell_infobox_height = configUtilities.get_config_value_as_integer(configfile=game_config, section='spellinfo',
                                                                            parameter='SI_DEPTH')
-        spell_info_top_left_corner = CommonUtils.get_ascii_to_unicode(game_config=game_config, parameter=ascii_prefix + 'TOP_LEFT')
+        spell_info_top_left_corner = CommonUtils.get_ascii_to_unicode(game_config=game_config,
+                                                                      parameter=ascii_prefix + 'TOP_LEFT')
 
         spell_info_bottom_left_corner = CommonUtils.get_ascii_to_unicode(game_config=game_config,
-                                                                        parameter=ascii_prefix + 'BOTTOM_LEFT')
+                                                                         parameter=ascii_prefix + 'BOTTOM_LEFT')
 
         spell_info_top_right_corner = CommonUtils.get_ascii_to_unicode(game_config=game_config,
-                                                                      parameter=ascii_prefix + 'TOP_RIGHT')
+                                                                       parameter=ascii_prefix + 'TOP_RIGHT')
 
         spell_info_bottom_right_corner = CommonUtils.get_ascii_to_unicode(game_config=game_config,
-                                                                         parameter=ascii_prefix + 'BOTTOM_RIGHT')
+                                                                          parameter=ascii_prefix + 'BOTTOM_RIGHT')
 
-        spell_info_horizontal = CommonUtils.get_ascii_to_unicode(game_config=game_config, parameter=ascii_prefix + 'HORIZONTAL')
-        spell_info_vertical = CommonUtils.get_ascii_to_unicode(game_config=game_config, parameter=ascii_prefix + 'VERTICAL')
+        spell_info_horizontal = CommonUtils.get_ascii_to_unicode(game_config=game_config,
+                                                                 parameter=ascii_prefix + 'HORIZONTAL')
+        spell_info_vertical = CommonUtils.get_ascii_to_unicode(game_config=game_config,
+                                                               parameter=ascii_prefix + 'VERTICAL')
 
         # render horizontal bottom
         for z in range(spell_infobox_start_x, (spell_infobox_start_x + spell_infobox_width)):
-            terminal.printf(x=z, y=(spell_infobox_start_y + spell_infobox_height), s=unicode_string_to_print + spell_info_horizontal + ']')
+            terminal.printf(x=z, y=(spell_infobox_start_y + spell_infobox_height),
+                            s=unicode_string_to_print + spell_info_horizontal + ']')
             terminal.printf(x=z, y=spell_infobox_start_y, s=unicode_string_to_print + spell_info_horizontal + ']')
 
         # render verticals
         for z in range(spell_infobox_start_y, (spell_infobox_start_y + spell_infobox_height) - 1):
             terminal.printf(x=spell_infobox_start_x, y=z + 1, s=unicode_string_to_print + spell_info_vertical + ']')
-            terminal.printf(x=(spell_infobox_start_x + spell_infobox_width), y=z + 1, s=unicode_string_to_print + spell_info_vertical + ']')
-
+            terminal.printf(x=(spell_infobox_start_x + spell_infobox_width), y=z + 1,
+                            s=unicode_string_to_print + spell_info_vertical + ']')
 
         # top left
-        terminal.printf(x=spell_infobox_start_x, y=spell_infobox_start_y, s=unicode_string_to_print + spell_info_top_left_corner + ']')
+        terminal.printf(x=spell_infobox_start_x, y=spell_infobox_start_y,
+                        s=unicode_string_to_print + spell_info_top_left_corner + ']')
         # bottom left
-        terminal.printf(x=spell_infobox_start_x, y=(spell_infobox_start_y + spell_infobox_height), s=unicode_string_to_print + spell_info_bottom_left_corner + ']')
+        terminal.printf(x=spell_infobox_start_x, y=(spell_infobox_start_y + spell_infobox_height),
+                        s=unicode_string_to_print + spell_info_bottom_left_corner + ']')
         # top right
-        terminal.printf(x=(spell_infobox_start_x + spell_infobox_width), y=spell_infobox_start_y, s=unicode_string_to_print + spell_info_top_right_corner + ']')
+        terminal.printf(x=(spell_infobox_start_x + spell_infobox_width), y=spell_infobox_start_y,
+                        s=unicode_string_to_print + spell_info_top_right_corner + ']')
         # bottom right
-        terminal.printf(x=(spell_infobox_start_x + spell_infobox_width), y=(spell_infobox_start_y + spell_infobox_height), s=unicode_string_to_print + spell_info_bottom_right_corner + ']')
-
-    def render_spell_info_panel(self, game_config):
-        image_y_scale = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
-                                                                    parameter='map_Yscale')
-        statusbox_height = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
-                                                                       parameter='STATUSBOX_HEIGHT')
-        player_entity = MobileUtilities.get_player_entity(gameworld=self.gameworld, game_config=game_config)
-
-        ac = 1
-        sc = 5
-        y = statusbox_height + 1
-
-        # spell bar slots are drawn first
-        for a in range(10):
-            terminal.put(x=(ac + a) * sc, y=y * image_y_scale, c=0xE500 + 0)
-        # now the spells based on the spell bar entities -- currently loads slots 1 - 6
-        slotid = 1
-        for spell_slot in range(6):
-            SpellUtilities.get_spell_entity_from_spellbar_slot(gameworld=self.gameworld, slot=slotid,
-                                                               player_entity=player_entity)
-            spell_image = 0
-
-            terminal.put(x=(ac + spell_slot) * sc, y=y * image_y_scale, c=0xE400 + spell_image)
-            slotid += 1
-
-        # and finally the utility spells
-
-    def render_player_vitals(self, game_config):
-
-        self.render_health(game_config=game_config)
-        self.render_mana(game_config=game_config)
-        self.render_special_power(game_config=game_config)
-
-    def render_health(self, game_config):
-
-        statusbox_height = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
-                                                                       parameter='STATUSBOX_HEIGHT')
-
-        player_entity = MobileUtilities.get_player_entity(self.gameworld, game_config)
-        current_health = MobileUtilities.get_mobile_derived_current_health(gameworld=self.gameworld,
-                                                                           entity=player_entity)
-        maximum_health = MobileUtilities.get_mobile_derived_maximum_health(gameworld=self.gameworld,
-                                                                           entity=player_entity)
-
-        str_to_print = "[color=red]Health[/color]"
-
-        self.render_bar(print_string=str_to_print, low_number=current_health, high_number=maximum_health,
-                        posy=statusbox_height + 3, posx=6, sprite_ref=0xE770, game_config=game_config)
-
-    def render_mana(self, game_config):
-
-        statusbox_height = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
-                                                                       parameter='STATUSBOX_HEIGHT')
-
-        player_entity = MobileUtilities.get_player_entity(self.gameworld, game_config)
-        current_mana = MobileUtilities.get_mobile_derived_current_mana(gameworld=self.gameworld, entity=player_entity)
-        max_mana = MobileUtilities.get_mobile_derived_maximum_mana(gameworld=self.gameworld, entity=player_entity)
-        str_to_print = "[color=blue]Mana[/color]"
-
-        self.render_bar(print_string=str_to_print, low_number=current_mana, high_number=max_mana,
-                        posy=statusbox_height + 4, posx=6, sprite_ref=0xE800, game_config=game_config)
-
-    def render_special_power(self, game_config):
-
-        statusbox_height = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
-                                                                       parameter='STATUSBOX_HEIGHT')
-
-        player_entity = MobileUtilities.get_player_entity(self.gameworld, game_config)
-        current_f1_power = MobileUtilities.get_mobile_derived_special_bar_current_value(gameworld=self.gameworld,
-                                                                                        entity=player_entity)
-        max_f1_power = MobileUtilities.get_mobile_derived_special_bar_max_value(gameworld=self.gameworld,
-                                                                                entity=player_entity)
-
-        str_to_print = "[color=green]Power[/color]"
-
-        self.render_bar(print_string=str_to_print, low_number=current_f1_power, high_number=max_f1_power,
-                        posy=statusbox_height + 5, posx=6, sprite_ref=0xE880, game_config=game_config)
+        terminal.printf(x=(spell_infobox_start_x + spell_infobox_width),
+                        y=(spell_infobox_start_y + spell_infobox_height),
+                        s=unicode_string_to_print + spell_info_bottom_right_corner + ']')
 
     @staticmethod
-    def render_bar(print_string, low_number, high_number, posy, posx, sprite_ref, game_config):
+    def render_equipped_weapons(gameworld,game_config):
 
-        image_y_scale = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
-                                                                    parameter='map_Yscale')
-        image_x_scale = configUtilities.get_config_value_as_integer(configfile=game_config, section='gui',
-                                                                    parameter='map_Xscale')
+        plpayer_entity = MobileUtilities.get_player_entity(gameworld=gameworld, game_config=game_config)
+        spell_infobox_start_x = configUtilities.get_config_value_as_integer(configfile=game_config, section='spellinfo',
+                                                                            parameter='SI_START_X')
 
-        display_percentage = formulas.calculate_percentage(low_number, high_number)
-        tens = int(display_percentage / 10)
-        units = display_percentage % 10
-        px = 0
+        spell_infobox_start_y = configUtilities.get_config_value_as_integer(configfile=game_config, section='spellinfo',
+                                                                            parameter='SI_START_Y')
+        equipped_weapons = MobileUtilities.get_weapons_equipped(gameworld=gameworld, entity=plpayer_entity)
+        print_col_no_item = "[color=SPELLINFO_NO_ITEM_EQUIPPED]"
+        print_col_item = "[color=SPELLINFO_ITEM_EQUIPPED]"
 
-        if print_string != "":
-            terminal.printf(4, posy * image_y_scale, print_string)
+        # title bar
+        terminal.printf(x=spell_infobox_start_x + 3, y=spell_infobox_start_y + 1, s='Weapons')
 
-        for a in range(tens):
-            terminal.put(x=(a + posx) * image_x_scale, y=posy * image_y_scale, c=sprite_ref + 0)
-            px += 1
+        # main hand
+        if equipped_weapons[0] == 0:
+            str_to_print = print_col_no_item + "Main:"
+        else:
+            str_to_print = print_col_item + "Main:" + ItemUtilities.get_item_name(gameworld=gameworld, entity=equipped_weapons[0])
+        terminal.printf(x=spell_infobox_start_x + 1, y=spell_infobox_start_y + 2, s=str_to_print)
 
-        if units > 0:
-            if units < 5:
-                terminal.put(x=(px + posx) * image_x_scale, y=posy * image_y_scale, c=sprite_ref + 3)
+        # off hand
+        if equipped_weapons[1] == 0:
+            str_to_print = print_col_no_item + "Off:"
+        else:
+            str_to_print = print_col_item + "Off:" + ItemUtilities.get_item_name(gameworld=gameworld, entity=equipped_weapons[1])
+        terminal.printf(x=spell_infobox_start_x + 1, y=spell_infobox_start_y + 3, s=str_to_print)
 
-            if units == 5:
-                terminal.put(x=(px + posx) * image_x_scale, y=posy * image_y_scale, c=sprite_ref + 2)
+        # both hands
+        if equipped_weapons[2] == 0:
+            str_to_print = print_col_no_item + "Both:"
+        else:
+            str_to_print = print_col_item + "Both:" + ItemUtilities.get_item_name(gameworld=gameworld, entity=equipped_weapons[2])
+        terminal.printf(x=spell_infobox_start_x + 1, y=spell_infobox_start_y + 4, s=str_to_print)
 
-            if units > 5:
-                terminal.put(x=(px + posx) * image_x_scale, y=posy * image_y_scale, c=sprite_ref + 1)
+    @staticmethod
+    def render_equipped_jewellery():
+        return True
+
+    @staticmethod
+    def render_health():
+        return True
+
+    @staticmethod
+    def render_mana():
+        return True
+
+    #
+    # formally known as the F1 bar
+    #
+    def render_class_mechanics(self):
+        return True
+
+    @staticmethod
+    def render_equipped_armour():
+        return True
+
+    def render_spell_bar(self):
+        return True
