08b54f107bc4c37de5fd0d6dab2d9815c902857f
Steven Devonport <steven.devonport@fastmail.com>
2020-10-24 13:54:39 +0100

    updated Json spell files

--- newGame/CreateSpells.py
+++ newGame/CreateSpells.py
@@ -34,45 +34,47 @@ class AsEntities:
 
         logger.debug('Creating spells as entities for {}', playable_class)
         thisspell = - 1
-        for spell in spell_file['spells']:
+        for spell in spell_file:
             thisspell = world.get_next_entity_id(gameworld=gameworld)
-            gameworld.add_component(thisspell, spells.Name(spell['name']))
-            gameworld.add_component(thisspell, spells.Description(spell['description']))
-            gameworld.add_component(thisspell, spells.ShortDescription(spell['short_description']))
-            gameworld.add_component(thisspell, spells.CastTime(spell['turns_to_cast']))
-            gameworld.add_component(thisspell, spells.CoolDown(int(spell['cool_down'])))
+            myspell = spell
+            logger.debug("myspell is:{}", myspell)
+            gameworld.add_component(thisspell, spells.Name(myspell['name']))
+            gameworld.add_component(thisspell, spells.Description(myspell['description']))
+            gameworld.add_component(thisspell, spells.ShortDescription(myspell['short_description']))
+            gameworld.add_component(thisspell, spells.CastTime(myspell['turns_to_cast']))
+            gameworld.add_component(thisspell, spells.CoolDown(int(myspell['cool_down'])))
             gameworld.add_component(thisspell, spells.ClassName(playable_class))
-            gameworld.add_component(thisspell, spells.SpellType(spell['type_of_spell']))
+            gameworld.add_component(thisspell, spells.SpellType(myspell['type_of_spell']))
             gameworld.add_component(thisspell, spells.StatusEffect(condis=[], boons=[], controls=[]))
-            gameworld.add_component(thisspell, spells.MaxTargets(spell['max_targets']))
-            spell_range_in_file = spell['spell_range']
+            gameworld.add_component(thisspell, spells.MaxTargets(myspell['max_targets']))
+            spell_range_in_file = myspell['spell_range']
             spell_range = configUtilities.get_config_value_as_integer(configfile=game_config, section='spells',
                                                                       parameter=spell_range_in_file.upper())
             gameworld.add_component(thisspell, spells.MaxRange(spell_range))
 
-            if spell['type_of_spell'] == 'combat':
-                gameworld.add_component(thisspell, spells.WeaponType(spell['weapon_type']))
-                gameworld.add_component(thisspell, spells.WeaponSlot(spell['weapon_slot']))
-                gameworld.add_component(thisspell, spells.DamageDuration(spell['damage_duration']))
-                gameworld.add_component(thisspell, spells.DamageCoefficient(spell['damage_coef']))
-                gameworld.add_component(thisspell, spells.GroundTargeted(spell['ground_targeted']))
-                gameworld.add_component(thisspell, spells.AreaOfEffect(use_area_of_effect=spell['aoe']))
-                if spell['aoe']:
+            if myspell['type_of_spell'] == 'combat':
+                gameworld.add_component(thisspell, spells.WeaponType(myspell['weapon_type']))
+                gameworld.add_component(thisspell, spells.WeaponSlot(myspell['weapon_slot']))
+                gameworld.add_component(thisspell, spells.DamageDuration(myspell['damage_duration']))
+                gameworld.add_component(thisspell, spells.DamageCoefficient(myspell['damage_coef']))
+                gameworld.add_component(thisspell, spells.GroundTargeted(myspell['ground_targeted']))
+                gameworld.add_component(thisspell, spells.AreaOfEffect(use_area_of_effect=myspell['aoe']))
+                if myspell['aoe']:
                     gameworld.add_component(thisspell,
-                                            spells.AreaOfEffectShape(area_of_effect_shape=spell['aoe_shape']))
+                                            spells.AreaOfEffectShape(area_of_effect_shape=myspell['aoe_shape']))
 
-            if spell['type_of_spell'] == 'heal':
-                spell_heal_file = spell['heal_duration']
+            if myspell['type_of_spell'] == 'heal':
+                spell_heal_file = myspell['heal_duration']
                 spell_heal = configUtilities.get_config_value_as_integer(configfile=game_config, section='spells',
                                                                          parameter='SPELL_HEAL_' + spell_heal_file.upper())
                 gameworld.add_component(thisspell, spells.HealingDuration(spell_heal))
-                gameworld.add_component(thisspell, spells.HealingCoef(float(spell['heal_coef'])))
+                gameworld.add_component(thisspell, spells.HealingCoef(float(myspell['heal_coef'])))
 
-            if spell['type_of_spell'] == 'utility':
+            if myspell['type_of_spell'] == 'utility':
                 gameworld.add_component(thisspell, spells.ItemLocation(spell['location']))
                 gameworld.add_component(thisspell, spells.ItemType(spell['item_type']))
 
-            effects = spell['effects']
+            effects = myspell['effects']
 
             addStatusEffects.process_status_effect(gameworld=gameworld, spell_entity=thisspell, effects=effects,
                                                    game_config=game_config, ch_class=playable_class)
