c5495568f7a14696051b5052385c7212405aaf9d
Steven Devonport <steven.devonport@fastmail.com>
2021-05-28 22:00:02 +0100

    Remove not needed AI routines

--- utilities/ai_utilities.py
+++ utilities/ai_utilities.py
@@ -1,11 +1,12 @@
 from utilities import mobileHelp
+from utilities.mobileHelp import MobileUtilities
 
 
 class AIUtilities:
 
     @staticmethod
     def can_i_move(gameworld, source_entity):
-        list_of_conditions = mobileHelp.MobileUtilities.get_current_condis_applied_to_mobile(gameworld=gameworld,
+        list_of_conditions = MobileUtilities.get_current_condis_applied_to_mobile(gameworld=gameworld,
                                                                                              entity=source_entity)
         if ['crippled', 'immobilize', 'stunned', 'dazed'] in list_of_conditions:
             return False
@@ -13,14 +14,36 @@ class AIUtilities:
             return True
 
     @staticmethod
-    def what_can_i_see_around_me(gameworld, source_entity):
-        # pending
-        pass
+    def what_can_i_see_around_me(gameworld, source_entity, game_map):
+        range_of_vision = MobileUtilities.get_mobile_senses_vision_range(gameworld=gameworld, entity=source_entity)
+        from_x = MobileUtilities.get_mobile_x_position(gameworld=gameworld, entity=source_entity)
+        from_y = MobileUtilities.get_mobile_y_position(gameworld=gameworld, entity=source_entity)
+
+        min_x_range = max(1, (from_x - range_of_vision))
+        max_x_range = min(game_map.width, (from_x + game_map.width))
+        min_y_range = max(1, (from_y - range_of_vision))
+        max_y_range = min(game_map.height, (from_y + game_map.height))
+        visible_entities = []
+
+        for across in range(min_x_range, max_x_range):
+            for down in range(min_y_range, max_y_range):
+                entity_id = game_map.tiles[across][down].entity
+                if entity_id > 0:
+                    visible_entities.append(entity_id)
+        MobileUtilities.set_visible_entities(gameworld=gameworld, target_entity=source_entity, visible_entities=visible_entities)
+
 
     @staticmethod
     def have_i_taken_damage(gameworld, source_entity):
-        # pending
-        pass
+
+        current_health = MobileUtilities.get_mobile_derived_current_health(gameworld=gameworld, entity=source_entity)
+        max_health = MobileUtilities.set_mobile_derived_max_health(gameworld=gameworld, entity=source_entity)
+
+        if current_health != max_health:
+            return True
+        else:
+            return False
+
 
     @staticmethod
     def can_i_cast_a_spell(gameworld, source_entity):
